d4c612b47327f6060d35954785ee83cf47143d3a
Ethan Schweinsberg <ees37@zips.uakron.edu>
2015-10-06 23:13:51 -0400

    added a sleep module and wrote some test code to vary a pwm signal on pin 18

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var sleep = require('sleep');
+var blaster = require('pi-blaster.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+  for (d = 0; d < 1; d+=0.1)
+  {
+    piblaster.setPwm(18,d);
+    sleep.sleep(4);
+  }
 });
 
 module.exports = router;
