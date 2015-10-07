d71502b7096308104ae6f34c417422ee3bfdd69b
root <root@raspberrypi.(none)>
2015-10-07 04:33:21 +0100

    debugging

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,9 @@ var blaster = require('pi-blaster.js');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
-  for (d = 0; d < 1; d+=0.1)
+  for (d = 0.1; d < 1; d+=0.1)
   {
+    console.log("hello");
     piblaster.setPwm(18,d);
     sleep.sleep(4);
     console.log("switching");
