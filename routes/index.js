42ba3286a2f0a605c8c7dbaf7f5d9a6663af2147
Ethan Schweinsberg <ees37@zips.uakron.edu>
2015-10-15 22:52:31 -0400

    Fix compile errors

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var blaster = require('pi-blaster.js');
-var snap = require('snapshot')
+var snap = require('snapshot/snapshot.js')
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
   snap.snapImage(function() {
-    
+
   });
 });
 
