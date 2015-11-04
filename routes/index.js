3fca1c7ddbc7626dfce10c8b8ef180e912ea294f
Ethan Schweinsberg <ees37@zips.uakron.edu>
2015-11-03 22:20:48 -0500

    attempt to serve image (fix)

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var blaster = require('pi-blaster.js');
 var snap = require('snapshot/snapshot.js')
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   snap.snapImage(function(snapshot) {
-    res.contentType('application/json');
+    //res.contentType('application/json');
     console.log("displayng images");
+    var img = fs.readFileSync(snapshot);
     //res.render('index', { photo: "/home/snapshot/" + snapshot });
     res.writeHead('200', {'Content-Type': 'image/png'});
     res.end("/home/snapshot/" + snapshot,'binary');
