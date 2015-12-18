ef20e20bb20208012c119bbfd738e7ea0d834311
Ethan Schweinsberg <ees37@zips.uakron.edu>
2015-12-18 01:12:53 -0500

    Began code for stitching images together

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/', function(req, res) {
   var today = dateformat(new Date(), "mm-dd-yyyy");
   var today_dir = PLANT_IMG_FULL_PATH + today;
 
+  snap.stitchPhotos();
   if (fs.existsSync(today_dir))
   {
       var plant_imgs = [];
